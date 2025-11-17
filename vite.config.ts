6c65c0b06a4c0eda8dcebed0176d0991a9b90209
Expertconnectionconsultant <abplaytv5@gmail.com>
2025-11-17 18:14:51 +0530

    Configure custom domain universalnetts.com for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/universal/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
